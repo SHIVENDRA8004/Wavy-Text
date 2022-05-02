0618e337372d9aa6cbbd7e2b4d1d07725c43ed2c
SHIVENDRA JAT <73995062+SHIVENDRA8004@users.noreply.github.com>
2022-05-02 22:44:46 +0530

    Uploaded HTML CSS Js Source Code Files

---
+++ index.js
@@ -0,0 +1,5 @@
+document.querySelector(".button").addEventListener("click", getText);
+function getText() {
+  var str = document.querySelector(".input").value;
+  for (var i = 0; i < str.length(); i++) {}
+}
