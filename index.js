6afc3e3d4b0c289180bf5ebd4e380b4716908559
Snehel <sahilrps31@gmail.com>
2022-05-04 01:48:35 +0530

    updated repo

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 document.querySelector(".button").addEventListener("click", getText);
 function getText() {
   var str = document.querySelector(".input").value;
-  for (var i = 0; i < str.length(); i++) {}
+  for (var i = 0; i < str.length(); i++) {
+    
+  }
 }
